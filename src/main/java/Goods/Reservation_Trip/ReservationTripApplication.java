8e0b31b530b3dfc5e8f501a3b1f6a3ec61cc4148
Yako <parkts4420@naver.com>
2025-03-25 17:13:50 +0900

    git init

---
+++ src/main/java/Goods/Reservation_Trip/ReservationTripApplication.java
@@ -0,0 +1,13 @@
+package Goods.Reservation_Trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationTripApplication.class, args);
+	}
+
+}
