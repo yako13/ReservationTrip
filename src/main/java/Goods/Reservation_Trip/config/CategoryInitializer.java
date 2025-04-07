c2f2ef0502bfa2b7849d3788f0afa38ea00c6d67
Yako <parkts4420@naver.com>
2025-04-07 09:10:15 +0900

    Feat: 카테고리 이름이 있는지 확인한 후 저장 기능

--- src/main/java/Goods/Reservation_Trip/config/CategoryInitializer.java
+++ src/main/java/Goods/Reservation_Trip/config/CategoryInitializer.java
@@ -8,41 +8,58 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CategoryInitializer implements CommandLineRunner{
+public class CategoryInitializer implements CommandLineRunner {
 
     private final PackageCategoryRepository packageCategoryRepository;
 
     // 카테고리 생성
     @Override
     public void run(String... args) {
-        if (packageCategoryRepository.count() == 0) {
-            // 대분류 depth:1
-            PackageCategory japan = new PackageCategory(null, "일본", null, 1);
-            packageCategoryRepository.save(japan);
-            PackageCategory china = new PackageCategory(null, "중국", null, 1);
-            packageCategoryRepository.save(china);
+        // 대분류 depth:1
+        PackageCategory japan = packageCategoryRepository.findByName("일본")
+                .orElseGet(() -> packageCategoryRepository.save(new PackageCategory(null, "일본", null, 1)));
 
-            // 중분류 depth:2
-            PackageCategory kansai = new PackageCategory(null, "간사이(관서)", japan, 2);
-            packageCategoryRepository.save(kansai);
-            PackageCategory kanto = new PackageCategory(null, "간토(관동)", japan, 2);
-            packageCategoryRepository.save(kanto);
+        PackageCategory china = packageCategoryRepository.findByName("중국")
+                .orElseGet(() -> packageCategoryRepository.save(new PackageCategory(null, "중국", null, 1)));
 
-            // 소분류 depth:3
+
+        // 중분류 depth:2
+        PackageCategory kansai = packageCategoryRepository.findByName("간사이(관서)")
+                .orElseGet(() -> packageCategoryRepository.save(new PackageCategory(null, "간사이(관서)", null, 2)));
+        PackageCategory kanto = packageCategoryRepository.findByName("간토(관동)")
+                .orElseGet(() -> packageCategoryRepository.save(new PackageCategory(null, "간토(관동)", null, 2)));
+
+
+        // 소분류 depth:3
+        if (!packageCategoryRepository.existsByName("오사카")) {
             PackageCategory osaka = new PackageCategory(null, "오사카", kansai, 3);
             packageCategoryRepository.save(osaka);
+        }
+        if (!packageCategoryRepository.existsByName("교토")) {
             PackageCategory kyoto = new PackageCategory(null, "교토", kansai, 3);
             packageCategoryRepository.save(kyoto);
+        }
+        if (!packageCategoryRepository.existsByName("고베")) {
             PackageCategory kobe = new PackageCategory(null, "고베", kansai, 3);
             packageCategoryRepository.save(kobe);
+        }
+        if (!packageCategoryRepository.existsByName("와카야마")) {
             PackageCategory wakayama = new PackageCategory(null, "와카야마", kansai, 3);
             packageCategoryRepository.save(wakayama);
+        }
+        if (!packageCategoryRepository.existsByName("도쿄")) {
             PackageCategory tokyo = new PackageCategory(null, "도쿄", kanto, 3);
             packageCategoryRepository.save(tokyo);
+        }
+        if (!packageCategoryRepository.existsByName("시즈오카")) {
             PackageCategory Shizuoka = new PackageCategory(null, "시즈오카", kanto, 3);
             packageCategoryRepository.save(Shizuoka);
+        }
+        if (!packageCategoryRepository.existsByName("하코네")) {
             PackageCategory Hakone = new PackageCategory(null, "하코네", kanto, 3);
             packageCategoryRepository.save(Hakone);
+        }
+        if (!packageCategoryRepository.existsByName("요코하마")) {
             PackageCategory Yokohama = new PackageCategory(null, "요코하마", kanto, 3);
             packageCategoryRepository.save(Yokohama);
         }
