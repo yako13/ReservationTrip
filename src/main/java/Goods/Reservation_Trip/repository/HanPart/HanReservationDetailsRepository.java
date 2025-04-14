f1926def16e23adaefc85638c17bdececc973d8f
Yako <parkts4420@naver.com>
2025-04-14 14:22:43 +0900

    Feat: reservation merge

--- src/main/java/Goods/Reservation_Trip/repository/HanPart/HanReservationDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package Goods.Reservation_Trip.repository.HanPart;
-
-import Goods.Reservation_Trip.entity.ReservationDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HanReservationDetailsRepository extends JpaRepository<ReservationDetails,Long> {
-}
